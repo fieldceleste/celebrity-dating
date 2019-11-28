0f9f81a1dda06360faa6dea219caf3897917ea1b
apple <apple@Celestes-IMac.local>
2019-11-27 20:06:35 -0800

    add content

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
     $("form#compatible").submit(function(event) {
-      var book = ($("select#book").val());
+      var book = $("select#book").val();
       var color = $("select#color").val();
-      var country =$("select#country").val();
       var gender = $("select#gender").val();
   
       if (book) {
-        var quote = (The Great Gatsby);
-        if (gender === 'male' && age < 26) {
-          quote += 50;
+        alert("yes!");
         }
+      
   
-        $("#rate").empty().append(quote);
-        $("#quote").show();
-      } else {
-        alert('Please enter your age.');
-      }
+      
+      
+        
   
       event.preventDefault();
     });
